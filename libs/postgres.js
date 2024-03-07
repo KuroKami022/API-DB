aab5860eec54c945e3b9f5d878a500d8a89b7e34
KuroKami022 <luisru693@gmail.com>
2024-03-07 16:08:01 -0500

    'finished'

--- libs/postgres.js
+++ libs/postgres.js
@@ -4,7 +4,7 @@ async function getConnection() {
   const client = new Client({
     host: 'localhost',
     port: 5432,
-    user: 'luis',
+    user: 'nico',
     password: 'admin123',
     database: 'my_store'
   });
--- services/user.service.js
+++ services/user.service.js
@@ -1,15 +1,22 @@
 const boom = require('@hapi/boom');
+
 const { models } = require('./../libs/sequelize');
+
 class UserService {
   constructor() {}
+
   async create(data) {
     const newUser = await models.User.create(data);
     return newUser;
   }
+
   async find() {
-    const rta = await models.User.findAll();
+    const rta = await models.User.findAll({
+      include: ['customer']
+    });
     return rta;
   }
+
   async findOne(id) {
     const user = await models.User.findByPk(id);
     if (!user) {
@@ -17,15 +24,18 @@ class UserService {
     }
     return user;
   }
+
   async update(id, changes) {
     const user = await this.findOne(id);
     const rta = await user.update(changes);
     return rta;
   }
+
   async delete(id) {
     const user = await this.findOne(id);
     await user.destroy();
     return { id };
   }
 }
+
 module.exports = UserService;
